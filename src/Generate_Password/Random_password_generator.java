51d09afa34dfee33af2a117d6deb276fc2a1d230
samibyrone <samsonibironke2@gmail.com>
2024-09-26 00:07:20 +0100

    random generate password

---
+++ src/Generate_Password/Random_password_generator.java
@@ -0,0 +1,43 @@
+package Generate_Password;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Random_password_generator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()-_+=<>?";
+    private static final int PASSWORD_LENGTH = 16;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String generate_password() {
+
+        Random random = new Random();
+
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+
+        for(int index = 0; index < PASSWORD_LENGTH; index ++) {
+
+            password.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+
+        }
+
+        return password.toString();
+    }
+
+
+    public static boolean is_generate_password_strong(String password) {
+
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&*()-_+=<>?]).{16,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(password);
+
+        return matcher.matches();
+    }
+
+}
---
+++ test/Generate_Password/Random_password_generatorTest.java
@@ -0,0 +1,57 @@
+package Generate_Password;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Random_password_generatorTest {
+
+    private Random_password_generator generator;
+
+    @BeforeEach
+    public void setUp() {
+        generator = new Random_password_generator();
+    }
+
+    @Test
+    public void test_for_password_check() {
+        String password = "samsonIbironke21";
+    }
+
+    @Test
+    public void password_generator_length_to_be_16_in_lengthTest() {
+        String password = generator.generate_password();
+        assertEquals(16, password.length(),"Password should not be more than 16 characters long");
+    }
+
+    @Test
+    public void password_generator_length_which_contains_upperCaseTest() {
+        String password = generator.generate_password();
+        assertEquals(password.matches(".*[A-Z].*"), true, "Password should contain at least minimum of one uppercase letter");
+    }
+
+    @Test
+    public void password_generator_length_which_contains_lowerCaseTest() {
+        String password = generator.generate_password();
+        assertEquals(password.matches(".*[a-z].*"), true, "Password should contain at least minimum of one lowercase letter");
+    }
+
+    @Test
+    public void password_generator_length_which_contains_digitsTest() {
+        String password = generator.generate_password();
+        assertEquals(password.matches(".*\\d.*"), true, "Password should contain at least minimum of one digit");
+    }
+
+    @Test void password_generator_length_which_contains_specialCharactersTest() {
+        String password = generator.generate_password();
+        assertEquals(password.matches(".*[@#$%^&*()-_+=<>?].*"), true, "Password should contain at least minimum of one special character");
+    }
+
+    @Test
+    public void password_generator_to_check_if_password_is_strongTest() {
+        String password = generator.generate_password();
+        boolean password_strength = generator.is_generate_password_strong(password);
+        assertEquals(true, password_strength, "Password should be strong");
+    }
+}
